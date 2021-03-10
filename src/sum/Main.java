7722c72bfdfdeecc94c2a6c0af18588292b84faa
Luis Miguel <luismiguelossaarias05@gmail.com>
2021-03-10 08:28:20 -0500

    First commit

---
+++ src/sum/Main.java
@@ -0,0 +1,30 @@
+package sum;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));	
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));	
+		
+		String numBooks;
+		String pricesEveryBook;
+		String [] pricesBooks;
+		
+		numBooks = br.readLine();
+		
+		int numBooksToPrice = Integer.parseInt(numBooks);
+		
+		pricesEveryBook = br.readLine();
+		
+		br.close();
+		bw.close();
+	}
+
+}
